2fc886976f728d84321007b49dd45847f327ef85
Timofey Prodanov <timofey.prodanov@gmail.com>
2022-04-12 17:23:30 +0200

    Update vcf evaluation

--- variants/eval_vars.sh
+++ variants/eval_vars.sh
@@ -6,66 +6,155 @@ wdir="$(dirname "$0")"
 
 USAGE="$(cat <<-END
 Evaluate diploid genotypes.
-    -i <dir>,   --input <dir>
-        Input directory. Must contain golden_phased.vcf.gz and bwa.bam.
+    -a <file>,   --alignment <file>
+        Alignment file (BAM or CRAM).
+    -g <file>,   --golden <file>
+        Golden vcf.gz file.
     -p <dir>,   --parascopy <dir>
         Parascopy directory.
-    -g <file>,  --golden <file>
-        Golden VCF file.
-    -o <dir>,   --output <dir>
-        Output directory. Must contain calling.bed file.
     -f <file>,   --fasta-ref <file>
         Fasta reference. Must contain <file>.sdf index.
+    -t <path> <path>,   --tools <path> <path>
+        Freebayes & GATK paths. Use "freebayes" and "gatk" if not set.
+    -r,  --rerun
+        Rerun everything.
+    -o <dir>,   --output <dir>
+        Output directory. Must contain calling.bed file.
 END
 )"
 
+freebayes="freebayes"
+gatk="gatk"
+rerun=false
+command="$0 $*"
+
 while (( "$#" )); do
     case "$1" in
-        -i|--input)
-            input="$2"
+        -a|--alignment)
+            alignment="$2"
+            shift 2
+            ;;
+        -g|--golden)
+            golden="$2"
             shift 2
             ;;
         -p|--parascopy)
-            par_dir="$2"
-            shift 2
-            ;;
-        -o|--output)
-            output="$2"
+            parascopy="$2"
             shift 2
             ;;
         -f|--fasta-ref)
             genome="$2"
             shift 2
             ;;
+        -o|--output)
+            output="$2"
+            shift 2
+            ;;
+        -t|--tools)
+            freebayes="$2"
+            gatk="$3"
+            shift 3
+            ;;
+        -r|--rerun)
+            rerun=true
+            shift 1
+            ;;
         -h|--help)
             echo "${USAGE}"
             exit 0
             ;;
         *)
-            echo "Error: Unexpected argument $1" >&2
+            >&2 echo "Error: Unexpected argument $1"
             exit 1
             ;;
     esac
 done
 
-par_dir="$(dirname $(ls ${par_dir}/**/variants.vcf.gz | head -1))"
+if [[ -z ${alignment} ]]; then
+    >&2 echo "Error: Alignment file (-a, --alignment) is not provided!"
+    exit 1
+elif [[ -z ${golden} ]]; then
+    >&2 echo "Error: Golden VCF file (-g, --golden) is not provided!"
+    exit 1
+elif [[ -z ${parascopy} ]]; then
+    >&2 echo "Error: Parascopy directory (-p, --parascopy) is not provided!"
+    exit 1
+elif [[ -z ${genome} ]]; then
+    >&2 echo "Error: Fasta reference (-f, --fasta-ref) is not provided!"
+    exit 1
+elif [[ -z ${output} ]]; then
+    >&2 echo "Error: Output directory (-o, --output) is not provided!"
+    exit 1
+fi
 
-rm -f ${output}/{parascopy,golden}.vcf.gz{,.tbi}
-rtg vcffilter --include-bed=${output}/calling.bed -i ${input}/golden_phased.vcf.gz -o ${output}/golden.vcf.gz
-rtg vcffilter --include-bed=${output}/calling.bed -i ${par_dir}/variants.vcf.gz -o ${output}/parascopy.vcf.gz
+regions="${output}/calling.bed"
+log="${output}/eval.log"
+echo "Command: $command" > ${log}
 
-~/Code/freebayes/build/freebayes -f ${genome} -t ${output}/calling.bed --genotype-qualities ${input}/bwa.bam | \
-    bgzip > ${output}/freebayes.vcf.gz
-tabix -p vcf ${output}/freebayes.vcf.gz
-~/Code/gatk-4.2.2.0/gatk HaplotypeCaller -I ${input}/bwa.bam -R ${genome} \
-    -L ${output}/calling.bed -O ${output}/gatk.vcf.gz
+if [[ ! -f ${regions} ]]; then
+    >&2 echo "Error: There is no file ${regions}!"
+    exit 1
+fi
 
-rm -rf ${output}/eval-{fb,gatk,paras}
-rtg vcfeval -b ${output}/golden.vcf.gz -c ${output}/freebayes.vcf.gz \
-    -t ${genome}.sdf -o ${output}/eval-fb > /dev/null
-rtg vcfeval -b ${output}/golden.vcf.gz -c ${output}/gatk.vcf.gz \
-    -t ${genome}.sdf -o ${output}/eval-gatk > /dev/null
-rtg vcfeval -b ${output}/golden.vcf.gz -c ${output}/parascopy.vcf.gz \
-    -t ${genome}.sdf -o ${output}/eval-paras > /dev/null
+par_name="$(basename ${parascopy})"
+if [[ ${rerun} = true ]]; then
+    rm -f ${output}/{golden,${par_name},freebayes,gatk}.vcf.gz{,.tbi}
+fi
 
-${wdir}/write_summary.py ${output}/eval-{fb,gatk,paras}
+if [[ ! -f ${output}/golden.vcf.gz ]]; then
+    >&2 echo "** Filtering Golden VCF file."
+    rtg vcffilter --include-bed=${regions} -i ${golden} -o ${output}/golden.vcf.gz &>> ${log}
+else
+    >&2 echo "!! Skipping Golden VCF file."
+fi
+
+if [[ ! -f ${output}/${par_name}.vcf.gz ]]; then
+    >&2 echo "** Filtering Parascopy VCF file."
+    rtg vcffilter --include-bed=${regions} -i ${parascopy}/variants.vcf.gz -o ${output}/${par_name}.vcf.gz &>> ${log}
+else
+    >&2 echo "!! Skipping Parascopy VCF file."
+fi
+
+if [[ ! -f ${output}/freebayes.vcf.gz ]]; then
+    >&2 echo "** Running Freebayes."
+    ${freebayes} -f ${genome} -t ${regions} --genotype-qualities ${alignment} 2>> ${log} > ${output}/freebayes.vcf
+    bgzip ${output}/freebayes.vcf
+    tabix -p vcf ${output}/freebayes.vcf.gz
+else
+    >&2 echo "!! Skipping Freebayes."
+fi
+
+if [[ ! -f ${output}/gatk.vcf.gz ]]; then
+    >&2 echo "** Running GATK."
+    ${gatk} HaplotypeCaller -I ${alignment} -R ${genome} -L ${regions} -O ${output}/gatk.vcf.gz &>> ${log}
+else
+    >&2 echo "!! Skipping GATK."
+fi
+
+if [[ ${rerun} = true ]]; then
+    rm -rf ${output}/eval-{${par_name},freebayes,gatk}
+fi
+
+if [[ ! -d ${output}/eval-freebayes ]]; then
+    >&2 echo "** Running Freebayes vcfeval."
+    rtg vcfeval -b ${output}/golden.vcf.gz -c ${output}/freebayes.vcf.gz \
+        -t ${genome}.sdf -e ${regions} -o ${output}/eval-freebayes &>> ${log}
+else
+    >&2 echo "!! Skipping Freebayes vcfeval."
+fi
+
+if [[ ! -d ${output}/eval-gatk ]]; then
+    >&2 echo "** Running GATK vcfeval."
+    rtg vcfeval -b ${output}/golden.vcf.gz -c ${output}/gatk.vcf.gz \
+        -t ${genome}.sdf -e ${regions} -o ${output}/eval-gatk &>> ${log}
+else
+    >&2 echo "!! Skipping GATK vcfeval."
+fi
+
+if [[ ! -d ${output}/eval-${par_name} ]]; then
+    >&2 echo "** Running Parascopy vcfeval."
+    rtg vcfeval -b ${output}/golden.vcf.gz -c ${output}/${par_name}.vcf.gz \
+        -t ${genome}.sdf -e ${regions} -o ${output}/eval-${par_name} &>> ${log}
+else
+    >&2 echo "!! Skipping Parascopy vcfeval."
+fi
---
+++ variants/expand_psvs.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+import sys
+import pysam
+import argparse
+import numpy as np
+
+import parascopy.inner.common as common
+import parascopy.inner.genome as genome_
+
+
+def process_psvs(vcf, out_header):
+    for var in vcf:
+        pos2 = var.info['pos2']
+        if 'fval' in var.info:
+            fvals = list(map(float, var.info['fval']))
+        else:
+            fvals = np.full(len(pos2) + 1, np.nan)
+        main_pos = '{}:{}'.format(var.chrom, var.pos)
+
+        rec = out_header.new_record()
+        rec.chrom = var.chrom
+        rec.start = var.start
+        rec.alleles = var.alleles
+        rec.info['fval'] = fvals[0]
+        rec.info['main'] = main_pos
+        yield rec
+
+        for i, pos in enumerate(pos2):
+            pos = pos.split(':')
+            strand = pos[2] == '+'
+            ref_ix = 1 if len(pos) == 3 else int(pos[3])
+
+            rec = out_header.new_record()
+            rec.chrom = pos[0]
+            rec.start = int(pos[1]) - 1
+            rec.ref = common.cond_rev_comp(var.alleles[ref_ix], strand=strand)
+            rec.alts = [common.cond_rev_comp(seq, strand=strand)
+                for j, seq in enumerate(var.alleles) if j != ref_ix]
+            rec.info['fval'] = fvals[i]
+            rec.info['main'] = main_pos
+            yield rec
+
+
+def create_header(genome):
+    vcf_header = pysam.VariantHeader()
+    vcf_header.add_line('##command="{}"'.format(' '.join(sys.argv)))
+
+    for name, length in zip(genome.chrom_names, genome.chrom_lengths):
+        vcf_header.add_line('##contig=<ID={},length={}>'.format(name, length))
+    vcf_header.add_line('##INFO=<ID=fval,Number=1,Type=Float,Description="Reference allele frequency">')
+    vcf_header.add_line('##INFO=<ID=main,Number=1,Type=String,Description="Main position">')
+    return vcf_header
+
+
+# def filter_overlaps(records):
+#     count = 0
+#     curr_start_ix = 0
+#     curr_chrom = None
+#     curr_end = None
+
+#     for i, rec in enumerate(records):
+#         if curr_chrom != rec.chrom or (curr_chrom == rec.chrom and curr_end > rec.start):
+#             if i - 1 > curr_start_ix:
+#                 count += i - curr_start_ix
+#                 for j in range(curr_start_ix, i):
+#                     records[j].info['fval'] = np.nan
+#             curr_chrom = rec.chrom
+#             curr_start_ix = i
+#             curr_end = rec.start + len(rec.ref)
+#         else:
+#             curr_end = max(curr_end, rec.start + len(rec.ref))
+
+#     if i - 1 > curr_start_ix:
+#         count += i - curr_start_ix
+#         for j in range(curr_start_ix, i):
+#             records[j].info['fval'] = np.nan
+#     sys.stderr.write('Filtered {:,} overlapping variants\n'.format(count))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', nargs='+', required=True,
+        help='Multiple input VCF files with PSVs.')
+    parser.add_argument('-f', '--fasta-ref', required=True,
+        help='Fasta reference file.')
+    parser.add_argument('-o', '--output', required=True,
+        help='Combined output VCF file.')
+    args = parser.parse_args()
+
+    genome = genome_.Genome(args.fasta_ref)
+    header = create_header(genome)
+
+    records = []
+    for filename in args.input:
+        records.extend(process_psvs(pysam.VariantFile(filename), header))
+    records.sort(key=lambda record: (genome.chrom_id(record.chrom), record.start))
+    # filter_overlaps(records)
+
+    with pysam.VariantFile(args.output, 'wz' if args.output.endswith('.gz') else 'w', header=header) as out_vcf:
+        for rec in records:
+            out_vcf.write(rec)
+
+    if args.output.endswith('.gz'):
+        common.Process(['tabix', '-p', 'vcf', args.output]).finish()
+
+
+if __name__ == '__main__':
+    main()
