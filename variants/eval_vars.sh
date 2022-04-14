1aadf1b0365a37ba6e004a1a17ef6723ec52e493
Timofey Prodanov <timofey.prodanov@gmail.com>
2022-04-14 19:38:28 +0200

    Update variants evaluation

--- variants/eval_vars.sh
+++ variants/eval_vars.sh
@@ -25,6 +25,7 @@ Evaluate diploid genotypes.
 END
 )"
 
+alignment=""
 freebayes="freebayes"
 gatk="gatk"
 rerun=false
@@ -76,10 +77,7 @@ while (( "$#" )); do
     esac
 done
 
-if [[ -z ${alignment} ]]; then
-    >&2 echo "Error: Alignment file (-a, --alignment) is not provided!"
-    exit 1
-elif [[ -z ${golden} ]]; then
+if [[ -z ${golden} ]]; then
     >&2 echo "Error: Golden VCF file (-g, --golden) is not provided!"
     exit 1
 elif [[ -z ${parascopy} ]]; then
@@ -99,13 +97,14 @@ fi
 
 set -u
 
+mkdir -p ${output}
 regions="${output}/calling.bed"
 log="${output}/eval.log"
 echo "Command: $command" > ${log}
 
 if [[ ! -f ${regions} ]]; then
-    >&2 echo "Error: There is no file ${regions}!"
-    exit 1
+    >& 2 echo "** Generating variant calling regions"
+    ${wdir}/get_call_regions.sh -i ${parascopy} -f ${genome} -o ${regions} &>> ${log}
 fi
 
 if [[ ${rerun} = true ]]; then
@@ -126,7 +125,7 @@ else
     >&2 echo "!! Skipping Parascopy VCF file."
 fi
 
-if [[ ! -f ${output}/freebayes.vcf.gz ]]; then
+if [[ -n ${alignment} ]] && [[ ! -f ${output}/freebayes.vcf.gz ]]; then
     >&2 echo "** Running Freebayes."
     ${freebayes} -f ${genome} -t ${regions} --genotype-qualities ${alignment} 2>> ${log} > ${output}/freebayes.vcf
     bgzip ${output}/freebayes.vcf
@@ -135,7 +134,7 @@ else
     >&2 echo "!! Skipping Freebayes."
 fi
 
-if [[ ! -f ${output}/gatk.vcf.gz ]]; then
+if [[ -n ${alignment} ]] && [[ ! -f ${output}/gatk.vcf.gz ]]; then
     >&2 echo "** Running GATK."
     ${gatk} HaplotypeCaller -I ${alignment} -R ${genome} -L ${regions} -O ${output}/gatk.vcf.gz &>> ${log}
 else
@@ -146,7 +145,7 @@ if [[ ${rerun} = true ]]; then
     rm -rf ${output}/eval-{${par_name},freebayes,gatk}
 fi
 
-if [[ ! -d ${output}/eval-freebayes ]]; then
+if [[ -f ${output}/gatk.vcf.gz ]] && [[ ! -d ${output}/eval-freebayes ]]; then
     >&2 echo "** Running Freebayes vcfeval."
     rtg vcfeval -b ${output}/golden.vcf.gz -c ${output}/freebayes.vcf.gz \
         -t ${genome}.sdf -e ${regions} -o ${output}/eval-freebayes &>> ${log}
@@ -154,7 +153,7 @@ else
     >&2 echo "!! Skipping Freebayes vcfeval."
 fi
 
-if [[ ! -d ${output}/eval-gatk ]]; then
+if [[ -f ${output}/gatk.vcf.gz ]] && [[ ! -d ${output}/eval-gatk ]]; then
     >&2 echo "** Running GATK vcfeval."
     rtg vcfeval -b ${output}/golden.vcf.gz -c ${output}/gatk.vcf.gz \
         -t ${genome}.sdf -e ${regions} -o ${output}/eval-gatk &>> ${log}
@@ -169,3 +168,6 @@ if [[ ! -d ${output}/eval-${par_name} ]]; then
 else
     >&2 echo "!! Skipping Parascopy vcfeval."
 fi
+
+printf "\n\n"
+${wdir}/write_summary.py ${output}/eval-*
